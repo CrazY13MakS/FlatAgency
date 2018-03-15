46881bb0222c588cabbbf3014655c7a245236177
Maks <crazy13maks@gmail.com>
2018-03-15 23:02:27 +0200

    Добавьте файлы проекта.

---
+++ FlatAgency/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FlatAgency.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
