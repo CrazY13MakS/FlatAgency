352a36d89ac68de2a3ba07c3803ea539c2e58202
Maks <crazy13maks@gmail.com>
2018-03-15 23:30:54 +0200

    Create models classes

---
+++ FlatAgency/Models/District.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlatAgency.Models
+{
+    public class District
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
---
+++ FlatAgency/Models/Flat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlatAgency.Models
+{
+    public class Flat
+    {
+        public int Id { get; set; }
+        public DateTime DateCreation { get; set; }
+        public DateTime DateDeletion { get; set; }
+        public decimal Price { get; set; }
+        public double Square { get; set; }
+        public District District { get; set; }
+        public String Address { get; set; }
+        public int Floor { get; set; }
+        public FlatClass Class { get; set; }
+    }
+}
